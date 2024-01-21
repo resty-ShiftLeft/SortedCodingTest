5e67a0807418e18b0623c56bd9f569a43654153b
resty-ShiftLeft <resty@theshiftleft.com>
2024-01-21 21:21:36 +0800

    Add NLog Config, Startup.cs and Modify Program.cs

--- Program.cs
+++ Program.cs
@@ -1,25 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
+using NLog;
+using NLog.Web;
+using SortedCodingTest;
 
-// Add services to the container.
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    logger.Info("Starting the Rainfall Api.");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    var startup = new Startup(builder.Configuration);
+    startup.ConfigureServices(builder.Services);
+    startup.ConfigureLogging(builder.Logging);
+    builder.Host.UseNLog();
+
+    var webApplication = builder.Build();
+
+    startup.ConfigureAppConfiguration(builder);
+    startup.Configure(webApplication, builder.Environment);
+
+    webApplication.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception exception)
+{
+    logger.Error(exception, "Stopped Rainfall Api program because of a start-up exception.");
+    throw;
+}
+finally
+{
+    LogManager.Shutdown();
+}
---
+++ Startup.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace SortedCodingTest
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public Startup(IConfigurationRoot configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllers(mvcOptions => mvcOptions.EnableEndpointRouting = false)
+            //    .AddNewtonsoftJson();
+
+            // Caching 
+            services.AddDistributedMemoryCache();
+            //services.AddCoreLayer(Configuration);
+
+            services.AddMvcCore(options =>
+            {
+                options.EnableEndpointRouting = false;
+
+            });
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAny",
+                    builder =>
+                    {
+                        builder
+                            .AllowAnyOrigin()
+                            //.WithOrigins("https://*.application")
+                            //.SetIsOriginAllowedToAllowWildcardSubdomains()
+                            .AllowAnyMethod()
+                            .AllowAnyHeader();
+                    });
+            });
+
+            services.AddAuthorization();
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Rainfall Api", Version = "v1" });
+            });
+
+
+            services.AddHsts(options =>
+            {
+                options.Preload = true;
+                options.IncludeSubDomains = true;
+                options.MaxAge = TimeSpan.FromHours(12);
+            });
+        }
+
+        public void ConfigureAppConfiguration(WebApplicationBuilder builder)
+        {
+            if (builder.Environment.IsDevelopment())
+            {
+                builder.Configuration
+                    .AddJsonFile(@"appsettings.Development.json", false, true)
+                    .AddEnvironmentVariables()
+                    .AddUserSecrets<Program>()
+                    .Build();
+            }
+            if (builder.Environment.IsProduction())
+            {
+                builder.Configuration
+                    .AddJsonFile(@"appsettings.json", false, true)
+                    .AddEnvironmentVariables()
+                    .AddUserSecrets<Program>()
+                    .Build();
+            }
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Rainfall Api"));
+            }
+            else
+            {
+                app.UseExceptionHandler("/error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+
+            if (env.IsDevelopment())
+            {
+                app.UseCors(x => x
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            }
+            else
+            {
+                app.UseCors("AllowAny");
+            }
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        public void ConfigureLogging(ILoggingBuilder loggingBuilder)
+        {
+            loggingBuilder.ClearProviders();
+            loggingBuilder.SetMinimumLevel(LogLevel.Trace);
+        }
+    }
+}
