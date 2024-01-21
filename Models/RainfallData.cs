ecaabc16c874bfd476e51fb9d64cad38e5ece3a2
resty-ShiftLeft <resty@theshiftleft.com>
2024-01-21 22:42:46 +0800

    Update Models Rainfaill data and RainfallfloodArea

--- Models/RainfallData.cs
+++ Models/RainfallData.cs
@@ -2,13 +2,14 @@
 {
     public class RainfallData
     {
-        public string id { get; set; }
+        public string @id { get; set; }
         public string description { get; set; }
         public string eaAreaName { get; set; }
         public string eaRegionName { get; set; }
 
-        public List<RainfallFloodAreaData>? rainfallFloodArea { get; set; } = new List<RainfallFloodAreaData>();
+        public RainfallFloodAreaData floodArea { get; set; } = new RainfallFloodAreaData();
 
+        public string floodAreaID { get; set; }
         public bool isTidal { get; set; }
         public string message { get; set; }
         public string severity { get; set; }
--- Models/RainfallFloodAreaData.cs
+++ Models/RainfallFloodAreaData.cs
@@ -2,8 +2,7 @@
 {
     public class RainfallFloodAreaData
     {
-        public string id { get; set; }
-        public string rainfallId { get; set; }
+        public string @id { get; set; }
         public string county { get; set; }
         public string notation { get; set; }
         public string polygon { get; set; }
