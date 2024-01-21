ddbdf09d8e6599a1d0d80fdf820cc9cdba2556ee
resty-ShiftLeft <resty@theshiftleft.com>
2024-01-21 22:52:07 +0800

    Added RainfallStationController initially

---
+++ Controllers/RainfallStationController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SortedCodingTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RainfallStationController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public RainfallStationController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+    }
+}
